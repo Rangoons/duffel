8e54ad796ecbe3a7e8f4357af5b47c7b2bed8c8b
Brendan McDonald <brendan.mcdonald@eagledream.com>
2019-06-07 14:27:58 -0400

    initial commit

--- src/constants/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: 'AIzaSyDwnCOSxrJaK7dXKzx4dUD3SgoggS_-ack',
-  authDomain: 'duffel-e3dab.firebaseapp.com',
-  databaseURL: 'https://duffel-e3dab.firebaseio.com',
-  projectId: 'duffel-e3dab',
-  storageBucket: 'duffel-e3dab.appspot.com',
-  messagingSenderId: '956847708835',
-};
